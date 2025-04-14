60aa842ca9bde083eb56ffad1b913b164c218e8d
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 11:14:04 +0100

    feat: add protected API routes for logout and retrieving authenticated user info

--- mail-master/routes/api.php
+++ mail-master/routes/api.php
@@ -13,3 +13,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+});
