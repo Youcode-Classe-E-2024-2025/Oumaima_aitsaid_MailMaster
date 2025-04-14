eda0287fbdcdd2a65d15ced9caac9b17ec1e0421
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 14:24:35 +0100

    feat: create EmailService to send campaigns with personalized content and tracking

---
+++ mail-master/app/Services/EmailService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Campaign;
+use App\Models\CampaignStat;
+use App\Models\Subscriber;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+
+class EmailService
+{
+   
+
+    
+
+   
+}
