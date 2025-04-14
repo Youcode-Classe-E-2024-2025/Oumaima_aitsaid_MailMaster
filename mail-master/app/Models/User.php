1611a2d4526b066a8788d60cbc94944b67c71d44
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 10:24:57 +0100

    feat: cast email_verified_at to datetime and hash password automatically

--- mail-master/app/Models/User.php
+++ mail-master/app/Models/User.php
@@ -26,7 +26,11 @@ class User extends Authenticatable
             'remember_token',
         ];
     
-      
+        protected $casts = [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    
         
     
 }
