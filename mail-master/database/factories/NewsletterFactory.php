14069a317427e4f3ce8a0bb86a80f3967a3a7373
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 15:09:31 +0100

    ajouter function definitio n in Factory Newsletter

--- mail-master/database/factories/NewsletterFactory.php
+++ mail-master/database/factories/NewsletterFactory.php
@@ -2,22 +2,20 @@
 
 namespace Database\Factories;
 
+use App\Models\Newsletter;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Newsletter>
- */
 class NewsletterFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Newsletter::class;
+
+    public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'name' => $this->faker->words(3, true) . ' Newsletter',
+            'description' => $this->faker->paragraph(),
         ];
     }
 }
