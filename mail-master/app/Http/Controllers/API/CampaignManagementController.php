b062a7d61a3f1d0087a92f25854e767d49bac579
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 14:27:20 +0100

    feat: implement sendCampaign method using EmailService with success/failure handling

--- mail-master/app/Http/Controllers/API/CampaignManagementController.php
+++ mail-master/app/Http/Controllers/API/CampaignManagementController.php
@@ -14,9 +14,28 @@ class CampaignManagementController extends Controller
 {
     protected $emailService;
 
-   
+    public function __construct(EmailService $emailService)
+    {
+        $this->emailService = $emailService;
+    }
 
-    
+    /**
+     * Send a campaign to all subscribers.
+     */
+    public function sendCampaign(Request $request, $id)
+    {
+        // Get newsletters belonging to the user
+        $newsletterIds = Newsletter::where('user_id', $request->user()->id)
+            ->pluck('id');
+        
+        $campaign = Campaign::whereIn('newsletter_id', $newsletterIds)
+            ->where('id', $id)
+            ->firstOrFail();
+
+        $result = $this->emailService->sendCampaign($campaign);
+
+        return response()->json($result, $result['success'] ? 200 : 400);
+    }
 
    
 
