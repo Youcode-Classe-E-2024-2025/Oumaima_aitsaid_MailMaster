830bff36aef40802889451b3feb760cb023764da
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 14:27:10 +0100

    feat: add CampaignManagementController to handle campaign sending, scheduling and stats

--- mail-master/app/Http/Controllers/API/CampaignManagementController.php
+++ mail-master/app/Http/Controllers/API/CampaignManagementController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Campaign;
+use App\Models\CampaignStat;
+use App\Models\Newsletter;
+use App\Services\EmailService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CampaignManagementController extends Controller
 {
-    //
+    protected $emailService;
+
+   
+
+    
+
+   
+
+   
 }
