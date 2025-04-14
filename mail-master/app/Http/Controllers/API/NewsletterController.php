be7925426954e2bb6135a8d173d1b9f769a149d2
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 12:12:31 +0100

    Create store function to add a new newsletter

--- mail-master/app/Http/Controllers/API/NewsletterController.php
+++ mail-master/app/Http/Controllers/API/NewsletterController.php
@@ -20,7 +20,28 @@ class NewsletterController extends Controller
 
         return response()->json($newsletters);
     }
-   
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $newsletter = Newsletter::create([
+            'user_id' => $request->user()->id,
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+
+        return response()->json([
+            'message' => 'Newsletter created successfully',
+            'newsletter' => $newsletter
+        ], 201);
+    }
 
    
 
