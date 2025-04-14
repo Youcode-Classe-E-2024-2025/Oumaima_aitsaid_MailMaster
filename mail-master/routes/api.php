06a1ac3f199f32e7e2f0bf2a889fa553a4585e95
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 13:48:54 +0100

    ajouter les routes de Subscribers+compaigns+newslettres

--- mail-master/routes/api.php
+++ mail-master/routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\NewsletterController;
+use App\Http\Controllers\API\SubscriberController;
+use App\Http\Controllers\API\
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,4 +20,14 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
+    Route::apiResource('newsletters', NewsletterController::class);
+    
+    // Subscriber routes
+    Route::apiResource('subscribers', SubscriberController::class);
+    Route::post('/subscribers/add-to-newsletter', [SubscriberController::class, 'addToNewsletter']);
+    Route::post('/subscribers/remove-from-newsletter', [SubscriberController::class, 'removeFromNewsletter']);
+    
+    // Campaign routes
+    Route::apiResource('campaigns', CampaignController::class);
+    Route::get('/campaigns/{id}/preview', [CampaignController::class, 'preview']);
 });
