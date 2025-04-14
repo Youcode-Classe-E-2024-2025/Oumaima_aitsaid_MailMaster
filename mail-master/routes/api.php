e9dfcff0334a39572430a65b9b7417aedd115854
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 11:13:38 +0100

    feat: add public API routes for user registration and login

---
+++ mail-master/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
