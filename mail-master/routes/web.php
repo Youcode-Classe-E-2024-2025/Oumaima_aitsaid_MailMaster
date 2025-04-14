a3d12a0bf38c53e8b1ece1822f08863c00280e7a
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 14:31:24 +0100

    Ajoutons les routes correspondantes dans routes/web.php

--- mail-master/routes/web.php
+++ mail-master/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\TrackingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/tracking/open/{token}', [TrackingController::class, 'trackOpen'])->name('campaigns.track');
+Route::get('/tracking/click/{token}', [TrackingController::class, 'trackClick'])->name('campaigns.track.click');
