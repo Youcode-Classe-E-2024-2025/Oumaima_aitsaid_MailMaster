8fbc466db7d77fe99ee525a645a0fe5b457feedc
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 15:09:55 +0100

    ajouter function definitio n in Factory Subscriber

--- mail-master/database/factories/SubscriberFactory.php
+++ mail-master/database/factories/SubscriberFactory.php
@@ -2,22 +2,29 @@
 
 namespace Database\Factories;
 
+use App\Models\Subscriber;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Subscriber>
- */
 class SubscriberFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Subscriber::class;
+
+    public function definition()
     {
         return [
-            //
+            'email' => $this->faker->unique()->safeEmail(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'status' => 'active',
         ];
+    }
+
+    public function unsubscribed()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'status' => 'unsubscribed',
+            ];
+        });
     }
 }
