1b30d56ce84e22a2b88990f18115e98ed51d44e2
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 10:51:11 +0100

    feat: define fillable fields for Campaign (newsletter_id, name, subject, content, status, scheduled_at, sent_at)

--- mail-master/app/Models/Campaign.php
+++ mail-master/app/Models/Campaign.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Campaign extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'newsletter_id',
+        'name',
+        'subject',
+        'content',
+        'status',
+        'scheduled_at',
+        'sent_at',
+    ];
+
+    protected $casts = [
+        'scheduled_at' => 'datetime',
+        'sent_at' => 'datetime',
+    ];
+
+    
+   
 }
