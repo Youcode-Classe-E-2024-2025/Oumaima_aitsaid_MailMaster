e1a721b0ebd72571d67eeae1072465e148044b9b
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 10:05:25 +0100

    update function de migration subscribers

--- mail-master/database/migrations/2025_04_14_090250_create_subscribers_table.php
+++ mail-master/database/migrations/2025_04_14_090250_create_subscribers_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('subscribers', function (Blueprint $table) {
             $table->id();
+            $table->string('email')->unique();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->enum('status', ['active', 'unsubscribed'])->default('active');
             $table->timestamps();
         });
     }
